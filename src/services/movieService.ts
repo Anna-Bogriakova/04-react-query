6586a24fc050be8789fadf01e2988a1b3a465f2f
Anna Bogriakova <anna.bogriakova@gmail.com>
2025-08-17 15:46:10 +0200

    initial commit

---
+++ src/services/movieService.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+import type { Movie } from "../types/movie";
+
+const BASE_URL = "https://api.themoviedb.org/3/search/movie";
+const TOKEN = import.meta.env.VITE_TMDB_TOKEN;
+
+export interface FetchMoviesResponse {
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+}
+
+export const fetchMovies = async (
+  query: string,
+  page: number
+): Promise<FetchMoviesResponse> => {
+  const response = await axios.get<FetchMoviesResponse>(BASE_URL, {
+    params: {
+      query,
+      page,
+    },
+    headers: {
+      Authorization: `Bearer ${TOKEN}`,
+    },
+  });
+  return response.data;
+};
